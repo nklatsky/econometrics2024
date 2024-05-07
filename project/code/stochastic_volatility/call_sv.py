13d2620999ac26639d988757644b10a8608207dc
nklatsky <nicholasklatsky@gmail.com>
2024-05-07 16:06:50 -0400

    sv push v1

---
+++ project/code/stochastic_volatility/call_sv.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+# Note: Filepaths are relative to the ECONOMETRICS2024 directory
+
+import os
+print("current working directory:", os.getcwd())
+
+import cmdstanpy
+# cmdstanpy.install_cmdstan()
+from cmdstanpy import CmdStanModel
+
+import numpy as np
+import pandas as pd
+
+
+data_path = "../data/fx_data_approx_percent_returns.csv"
+data = pd.read_csv(data_path)
+
+# Define dependent variable
+y = data["DXY"].values
+
+stan_filepath = "sv.stan"
+
+# Load Stan model
+model = CmdStanModel(stan_file=stan_filepath)
+
+# Prepare data for Stan model
+data = {
+    'T': len(y),
+    'y': y
+}
+
+# Fit the model
+fit = model.sample(data=data)
+
+# Print summary of the model fit
+print(fit.summary())
+# Print diagnosis
+print(fit.diagnose())
+
+# Error checking
+# Access the samples for the parameters of interest
+h_samples = fit.stan_variable("h")
+sigma_samples = fit.stan_variable("sigma")
+scale_samples = fit.stan_variable("scale")
+#
+
+# Save samples to CSV 
+# Create folder to store sampler outputs (if it doesn't exist)
+os.makedirs("outputs", exist_ok=True)
+
+# convert to DataFrame
+h_samples = pd.DataFrame(h_samples)
+sigma_samples = pd.DataFrame(sigma_samples)
+scale_samples = pd.DataFrame(scale_samples)
+
+#Write to CSV
+h_samples.to_csv("outputs/h_samples.csv", index=False)
+sigma_samples.to_csv("outputs/sigma_samples.csv", index=False)
+scale_samples.to_csv("outputs/scale_samples.csv", index=False)
+
--- project/code/stochastic_volatility/sv.stan
+++ project/code/stochastic_volatility/sv.stan
@@ -11,7 +11,7 @@ data {
 parameters {
   real mu;                     // mean log volatility
   real<lower=-1, upper=1> phi; // persistence of volatility (auto-regression coefficient)
-  real<lower=0> sigma;         // white noise shock scale
+  real<lower=0.01> sigma;         // white noise shock scale (set lower bound slightly above zero to avoid computational issues)
   vector[T] h_std;             // std log volatility time t (for speed optimization)
 }
 
@@ -22,6 +22,8 @@ transformed parameters {
   h += mu;
   for (t in 2:T)
     h[t] += phi * (h[t-1] - mu);
+
+  vector[T] scale = exp(h / 2) + 1e-10 ;  // Compute scale parameters for the normal distribution (in transformed parameters block to enable inspection) ((also add 1e-10 to avoid errors))
 }
 
 
@@ -29,12 +31,12 @@ model {
 
   phi ~ normal(0.90, 0.1) T[-1, 1];  // Truncated Normal Prior (formerly: phi ~ uniform(-1, 1);)
 
-  sigma ~ sigma ~ inv_gamma(3, 2);   // Formerly cauchy(0, 5); 
+  sigma ~ inv_gamma(3, 2);   // Formerly cauchy(0, 5); 
 
   mu ~  std_normal();                // Formerly cauchy(0, 10);
 
   h_std ~ std_normal();              // (for speed optimization)
   
-  y ~ normal(0, exp(h / 2));         // Vectorized
+  y ~ normal(0, scale);         // Vectorized
 
 }
